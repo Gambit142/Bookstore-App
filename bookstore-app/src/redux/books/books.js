a31557cacc7c8046f6fc8f8f14a82520b08c0abe
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-01 20:52:02 +0100

    Add Delete button to the BooksPage component

--- bookstore-app/src/redux/books/books.js
+++ bookstore-app/src/redux/books/books.js
@@ -5,6 +5,7 @@ const BooksPage = () => (
     <div className="books-container d-flex">
       <h2>Name of Book</h2>
       <p>The Merchant of Venice</p>
+      <button type="button">Delete</button>
     </div>
     <input type="text" name="addBooks" placeholder="Book Title" />
   </>
