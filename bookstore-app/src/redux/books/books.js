166c5ceb9fd85b304c83a0b8752066e82b317c20
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-01 20:48:03 +0100

    Revert "Add Delete button to the BooksPage component in the books.js file"
    This reverts commit 6250a6c30fab441441885f49a024f6952b3ec6f0.

--- bookstore-app/src/redux/books/books.js
+++ bookstore-app/src/redux/books/books.js
@@ -5,7 +5,6 @@ const BooksPage = () => (
     <div className="books-container d-flex">
       <h2>Name of Book</h2>
       <p>The Merchant of Venice</p>
-      <button type="button">Delete</button>
     </div>
     <input type="text" name="addBooks" placeholder="Book Title" />
   </>
