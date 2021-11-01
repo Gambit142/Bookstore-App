e8962793f0aa988a88867d1b7921fbe5d051d03e
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-01 20:10:38 +0100

    Create the books page in books.js file

---
+++ bookstore-app/src/redux/books/books.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BooksPage = () => (
+  <>
+    <div>
+      <h2>Name of Book</h2>
+      <p>The Merchant of Venice</p>
+    </div>
+    <input type="text" name="addBooks" placeholder="Book Title" />
+  </>
+);
+
+export default BooksPage;
