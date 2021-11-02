0aad589128b7e4b3ac5fe8fcf9ab7b05f16d7f28
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-02 14:25:53 +0100

    Make store available to components in the project

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import BooksPage from '../redux/books/books';
+import BooksPage from './bookpage';
 import CategoriesPage from '../redux/categories/categories';
 import Header from './header';
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './redux/configureStore';
 import './index.css';
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider bookStore={store}>
+      <App />
+    </Provider>
   </Router>,
   document.getElementById('root'),
 );
