3eeaedda32508dc42a1b70c56fce4e3805173f31
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-03 23:01:33 +0100

    Add redux thunk as a middleware in the index.js file

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,16 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import booksReducer from './books/books';
 
 const reducer = combineReducers({
   booksReducer,
 });
+const middleware = [logger, thunk];
 
 const store = createStore(
   reducer,
-  applyMiddleware(logger),
+  applyMiddleware(...middleware),
 );
 
 export default store;
