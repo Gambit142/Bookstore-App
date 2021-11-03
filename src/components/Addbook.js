b7d2589f9da7fd5777ec72e4a298a93a13890999
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-03 12:21:12 +0100

    Add select form control for the category section

--- src/components/Addbook.js
+++ src/components/Addbook.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/books';
+import createId from './apiCalls';
 
 const AddBook = () => {
   const dispatch = useDispatch();
@@ -39,6 +40,15 @@ const AddBook = () => {
         value={author}
         placeholder="Book Author"
       />
+      <select name="categories" id="books-categories">
+        <option value="default">---</option>
+        <option value="Action">Action</option>
+        <option value="Comic">Comic</option>
+        <option value="Romantic">Romantic</option>
+        <option value="Literary Fiction">Literary Fiction</option>
+        <option value="Historical Fiction">Historical Fiction</option>
+        <option value="Detective and Mystery">Detective and Mystery</option>
+      </select>
       <button type="submit">Add Books</button>
     </form>
   );
