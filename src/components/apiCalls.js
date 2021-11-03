e05fe674f5911f290186a2fe536ec020673f0401
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-03 23:00:04 +0100

    Create functions that create the Api ids, POST data to the API and DELETE books in the API

--- src/components/apiCalls.js
+++ src/components/apiCalls.js
@@ -1,11 +1,27 @@
-const APIURl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/LWx5miYaJfjkFh9ASFAf/books';
-const createId = async () => {
+export const APIURl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/LWx5miYaJfjkFh9ASFAf/books';
+
+const createBook = async (id, titleName, categoryName) => {
   const response = await fetch(APIURl, {
     method: 'POST',
+    body: JSON.stringify({ item_id: id, title: titleName, category: categoryName }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
   });
   const DATA = await response.text();
-  console.log(DATA);
   return DATA;
 };
 
-export default createId;
+export const deleteBookfromApi = async (id) => {
+  const response = await fetch(`${APIURl}/${id}`, {
+    method: 'DELETE',
+    body: JSON.stringify({ item_id: id }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const DATA = await response.text();
+  return DATA;
+};
+
+export default createBook;
