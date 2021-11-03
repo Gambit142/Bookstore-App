43daac96468bea5b8aea0bceedaa5badc368746c
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-03 23:05:01 +0100

    Refactor the way the list of books are rendered in the ReactDOM

--- src/components/booklist.js
+++ src/components/booklist.js
@@ -2,17 +2,20 @@ import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/books';
 
 const IndividualBook = (prop) => {
-  const { title, author, id } = prop;
+  const { data } = prop;
+  const [id, [{ title, category }]] = data;
   const dispatch = useDispatch();
-  const removeBookFromStore = (id) => {
+  const removeBookFromStore = () => {
     dispatch(removeBook(id));
   };
   return (
-    <div>
-      {title}
-      {author}
-      <button type="button" onClick={() => removeBookFromStore(id)}>Delete</button>
-    </div>
+    <>
+      <div>
+        {title}
+        {category}
+      </div>
+      <button type="submit" onClick={removeBookFromStore}>Delete</button>
+    </>
   );
 };
 
