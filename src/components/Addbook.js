8ae17811163a3826cc24726791dd5c722d1f623c
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-05 12:55:20 +0100

    Restructure the form controls to make it easy to style according to the template

--- src/components/Addbook.js
+++ src/components/Addbook.js
@@ -7,7 +7,10 @@ const randomAuthors = ['Toni Morrison', 'Charles Dickens',
   'Mark Twain', 'J.K Rawlings',
   'William Shakespeare', 'Dan Brown',
   'Virginia Woolfe', 'Suzanne Collins', 'Frank Herbert',
+  'Wole Soyinka',
 ];
+
+const Chapters = ['Chapter 4: The Getaway', 'Introduction', 'Chapter 9: A lesson Learned', 'Chapter 2: The Dropoff'];
 
 const AddBook = () => {
   const dispatch = useDispatch();
@@ -22,6 +25,8 @@ const AddBook = () => {
       title,
       category,
       author: randomAuthors[Math.floor(Math.random() * randomAuthors.length)],
+      percentage: Math.round(Math.random() * 100),
+      chapters: Chapters[Math.floor(Math.random() * Chapters.length)],
     };
     dispatch(addBook(newBook));
     setTitle('');
@@ -29,25 +34,31 @@ const AddBook = () => {
   };
 
   return (
-    <form onSubmit={submitBookToStore} className="form-container">
-      <input
-        type="text"
-        name="title"
-        id="title"
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
-        placeholder="Book Title"
-      />
-      <select onChange={(e) => setCategory(e.target.value)} value={category} name="categories" id="books-categories">
-        <option value="default">---</option>
-        <option value="Action">Action</option>
-        <option value="Comic">Comic</option>
-        <option value="Romantic">Romantic</option>
-        <option value="Literary Fiction">Literary Fiction</option>
-        <option value="Historical Fiction">Historical Fiction</option>
-        <option value="Detective and Mystery">Detective and Mystery</option>
-      </select>
-      <button type="submit">Add Books</button>
+    <form onSubmit={submitBookToStore} className="form-container d-flex">
+      <hr />
+      <h2> ADD NEW BOOK </h2>
+      <div className="form-inputs d-flex">
+        <label htmlFor="title">
+          <input
+            type="text"
+            name="title"
+            id="title"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            placeholder="Book Title"
+          />
+        </label>
+        <select onChange={(e) => setCategory(e.target.value)} value={category} name="categories" id="books-categories">
+          <option value="default">Category</option>
+          <option value="Action">Action</option>
+          <option value="Comic">Comic</option>
+          <option value="Romantic">Romantic</option>
+          <option value="Literary Fiction">Literary Fiction</option>
+          <option value="Historical Fiction">Historical Fiction</option>
+          <option value="Detective and Mystery">Detective and Mystery</option>
+        </select>
+        <button className="submit-btn" type="submit">Add Books</button>
+      </div>
     </form>
   );
 };
